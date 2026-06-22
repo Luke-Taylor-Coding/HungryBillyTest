1f8bfb026ab2173df061c284eea4e28424eac029
Luke.Taylor <luketaylor1702@gmail.com>
2026-06-22 11:30:52 +0100

    Basic player dampening - contains errors
    There is now a basic player dampening to give a spaceship like effect however the planet gravity becomes incredibly strong due to its effect also applying to the dampening

--- Assets/Scripts/Player/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement/PlayerMovement.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
-// class for baisc player movement logic
 
-public class Player : MonoBehaviour
+// class for basic player movement logic
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float m_moveSpeed = 5f;
+    [SerializeField] private float m_moveDampening = 0.05f;
+
     private Rigidbody2D m_rb;
     private Vector2 m_movement;
+    private Vector2 m_targetVelocity;
+    private Vector2 m_refVelocity = Vector2.zero;
 
-    private void Start()
+    private void Awake()
     {
         m_rb = GetComponent<Rigidbody2D>();
     }
@@ -17,10 +21,14 @@ public class Player : MonoBehaviour
         // get input using the new input system (WASD or Arrow Keys)
         m_movement.x = Input.GetAxisRaw("Horizontal");
         m_movement.y = Input.GetAxisRaw("Vertical");
+
+        // Define the target velocity based on input
+        m_targetVelocity = m_movement.normalized * m_moveSpeed;
     }
 
     private void FixedUpdate()
     {
-        m_rb.MovePosition(m_rb.position + m_movement.normalized * m_moveSpeed * Time.fixedDeltaTime);
+        // Smoothly change the current velocity towards the target velocity.
+        m_rb.linearVelocity = Vector2.SmoothDamp(m_rb.linearVelocity, m_targetVelocity, ref m_refVelocity, m_moveDampening);
     }
 }
