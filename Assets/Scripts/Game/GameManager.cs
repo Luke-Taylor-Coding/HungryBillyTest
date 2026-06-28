5c73868b326c2e595fe6e348987db443f5c43b83
Luke.Taylor <luketaylor1702@gmail.com>
2026-06-28 16:45:49 +0100

    win and death screens
    Instead of splitting win and death screens into their own scenes I've built in UI overlays that occur based on the game manager events that determine win or death.
    
    On these events game objects such as the player, game UI and controls are disabled to display the win/death screen and prevent further player interactions

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 // A class to manage the game and its state / events such as on player death or game win
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private UnityEvent m_onPlayerDeath;
+    [SerializeField] private UnityEvent m_onGameWin;
+
     public void OnPlayerDeath()
     {
 #if UNITY_EDITOR
         Debug.Log("GAME_MANAGER: Player has died. Game Over.");
 #endif
-        // Implement game over logic here
+        m_onPlayerDeath.Invoke();
     }
 
     public void OnGameWin()
@@ -18,5 +22,6 @@ public class GameManager : MonoBehaviour
         Debug.Log("GAME_MANAGER: Player has won the game!");
 #endif
         // Implement game win logic here
+        m_onGameWin.Invoke();
     }
 }
